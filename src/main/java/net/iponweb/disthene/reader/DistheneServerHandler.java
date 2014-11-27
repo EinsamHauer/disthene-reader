a764de86c23b40c064c3063f4acf717c6929ee96
Andrei.B.Ivanov@gmail.com <Andrei.B.Ivanov@gmail.com>
2014-11-27 03:35:00 +0300

    Initial structure + http server

---
+++ src/main/java/net/iponweb/disthene/reader/DistheneServerHandler.java
@@ -0,0 +1,51 @@
+package net.iponweb.disthene.reader;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.*;
+import static io.netty.handler.codec.http.HttpResponseStatus.CONTINUE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+/**
+ * @author Andrei Ivanov
+ */
+public class DistheneServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof HttpRequest) {
+            HttpRequest req = (HttpRequest) msg;
+
+            if (DefaultHttpHeaders.is100ContinueExpected(req)) {
+                ctx.write(new DefaultFullHttpResponse(HTTP_1_1, CONTINUE));
+            }
+            boolean keepAlive = DefaultHttpHeaders.isKeepAlive(req);
+            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer("Hi from Disthene".getBytes()));
+            response.headers().set(CONTENT_TYPE, "application/json");
+            response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+
+            if (!keepAlive) {
+                ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+            } else {
+                response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+                ctx.write(response);
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/net/iponweb/disthene/reader/Main.java
@@ -0,0 +1,44 @@
+package net.iponweb.disthene.reader;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    final static Logger logger = Logger.getLogger(Main.class);
+    private static int port = 9080;
+    private static Channel channel = null;
+    private static EventLoopGroup bossGroup = new NioEventLoopGroup(10);
+    private static EventLoopGroup workerGroup = new NioEventLoopGroup(10);
+
+    public static void main(String[] args) throws Exception {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.option(ChannelOption.SO_BACKLOG, 1024);
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new DisthenServerInitializer());
+
+        channel = serverBootstrap.bind(port).sync().channel();
+        logger.info("Listening on port " + port);
+
+        System.out.println("Hello World!");
+    }
+
+    public static void shutdown() {
+        ChannelFuture f = channel.close();
+        f.awaitUninterruptibly();
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+}
