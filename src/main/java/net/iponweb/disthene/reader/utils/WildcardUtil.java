38244793c1d71235f4763b03f682db40f7f803b0
aivanov <andrei.b.ivanov@gmail.com>
2015-07-30 20:26:22 +0300

    fixing EinsamHauer/disthene-reader/#1

--- src/main/java/net/iponweb/disthene/reader/utils/WildcardUtil.java
+++ src/main/java/net/iponweb/disthene/reader/utils/WildcardUtil.java
@@ -1,23 +1,13 @@
 package net.iponweb.disthene.reader.utils;
 
-import org.apache.commons.lang.StringUtils;
-
 /**
  * @author Andrei Ivanov
  */
 public class WildcardUtil {
 
-    public static String getRegExFromWildcard(String wildcard) {
-        return wildcard.replace(".", "\\.").replace("*", ".*").replace("{", "(")
-                .replace("}", ")").replace(",", "|");
-    }
-
     public static String getPathsRegExFromWildcard(String wildcard) {
         return wildcard.replace(".", "\\.").replace("*", "[^\\.]*").replace("{", "(")
-                .replace("}", ")").replace(",", "|");
+                .replace("}", ")").replace(",", "|").replace("?", "[^\\.]");
     }
 
-    public static int getPathDepth(String wildcard) {
-        return StringUtils.countMatches(wildcard, ".") + 1;
-    }
 }
