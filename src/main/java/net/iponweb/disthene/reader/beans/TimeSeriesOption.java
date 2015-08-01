8f9884434fb35a93356153e26fc2ce94da5ce2c2
Andrei Ivanov <Andrei.B.Ivanov@gmail.com>
2015-08-01 03:37:16 +0300

    fixed summarize function

--- src/main/java/net/iponweb/disthene/reader/beans/TimeSeriesOption.java
+++ src/main/java/net/iponweb/disthene/reader/beans/TimeSeriesOption.java
@@ -4,5 +4,5 @@ package net.iponweb.disthene.reader.beans;
  * @author Andrei Ivanov
  */
 public enum TimeSeriesOption {
-    SECOND_Y_AXIS, COLOR, DRAW_AS_INFINITE, STACKED, LINE_WIDTH, INVISIBLE, ALPHA, DASHED, CONSOLIDATE_BY_FUNCTION;
+    SECOND_Y_AXIS, COLOR, DRAW_AS_INFINITE, STACKED, LINE_WIDTH, INVISIBLE, ALPHA, DASHED;
 }
--- src/main/java/net/iponweb/disthene/reader/graphite/functions/SummarizeFunction.java
+++ src/main/java/net/iponweb/disthene/reader/graphite/functions/SummarizeFunction.java
@@ -28,6 +28,8 @@ public class SummarizeFunction extends DistheneFunction {
         // parse interval
         int step = (int) Math.abs(DateTimeUtils.parseTimeOffset((String) arguments.get(1)));
 
+        String aggregation = arguments.size() > 2 ? ((String) arguments.get(2)).toLowerCase().replaceAll("[\"\']", "") : "sum";
+
         List<TimeSeries> processedArguments = new ArrayList<>();
         processedArguments.addAll(evaluator.eval((Target) arguments.get(0)));
 
@@ -50,7 +52,28 @@ public class SummarizeFunction extends DistheneFunction {
             int index = 0;
             while (ts.getFrom() + index * ts.getStep() <= ts.getTo()) {
                 if ((ts.getFrom() + index * ts.getStep()) % step == 0 && (ts.getFrom() + index * ts.getStep()) != from) {
-                    consolidated.add(CollectionUtils.sum(buffer));
+                    switch (aggregation) {
+                        case "last": {
+                            consolidated.add(CollectionUtils.last(buffer));
+                            break;
+                        }
+                        case "avg": {
+                            consolidated.add(CollectionUtils.average(buffer));
+                            break;
+                        }
+                        case "sum": {
+                            consolidated.add(CollectionUtils.sum(buffer));
+                            break;
+                        }
+                        case "min": {
+                            consolidated.add(CollectionUtils.min(buffer));
+                            break;
+                        }
+                        case "max": {
+                            consolidated.add(CollectionUtils.max(buffer));
+                            break;
+                        }
+                    }
                     buffer.clear();
                 }
                 buffer.add(ts.getValues()[index]);
@@ -58,7 +81,28 @@ public class SummarizeFunction extends DistheneFunction {
                 index++;
             }
 
-            consolidated.add(CollectionUtils.sum(buffer));
+            switch (aggregation) {
+                case "last": {
+                    consolidated.add(CollectionUtils.last(buffer));
+                    break;
+                }
+                case "avg": {
+                    consolidated.add(CollectionUtils.average(buffer));
+                    break;
+                }
+                case "sum": {
+                    consolidated.add(CollectionUtils.sum(buffer));
+                    break;
+                }
+                case "min": {
+                    consolidated.add(CollectionUtils.min(buffer));
+                    break;
+                }
+                case "max": {
+                    consolidated.add(CollectionUtils.max(buffer));
+                    break;
+                }
+            }
 
             ts.setFrom(from);
             ts.setTo(to);
@@ -71,8 +115,16 @@ public class SummarizeFunction extends DistheneFunction {
 
     @Override
     public void checkArguments() throws InvalidArgumentException {
-        if (arguments.size() != 2) throw new InvalidArgumentException("summarize: number of arguments is " + arguments.size() + ". Must be two.");
+        if (arguments.size() > 4 || arguments.size() < 2) throw new InvalidArgumentException("summarize: number of arguments is " + arguments.size() + ". Must be two or three.");
         if (!(arguments.get(0) instanceof Target)) throw new InvalidArgumentException("summarize: argument is " + arguments.get(0).getClass().getName() + ". Must be series");
         if (!DateTimeUtils.testTimeOffset((String) arguments.get(1))) throw new InvalidArgumentException("summarize: interval cannot be parsed (" + arguments.get(1) + ")");
+
+        if (arguments.size() > 2) {
+            if (!(arguments.get(2) instanceof String)) throw new InvalidArgumentException("summarize: argument is " + arguments.get(2).getClass().getName() + ". Must be a string");
+            String argument = ((String) arguments.get(2)).toLowerCase().replaceAll("[\"\']", "");
+            if (!argument.equals("last") && !argument.equals("avg") && !argument.equals("sum") && !argument.equals("min") && !argument.equals("max")) {
+                throw new InvalidArgumentException("summarize: must be aggregation.");
+            }
+        }
     }
 }
