28ea18ad767f7c82cf0ad7cac92e770b97911831
Patryk Gałczyński <patryk.galczynski@dreamlab.pl>
2018-11-06 23:38:10 +0100

    conflicts resolve

--- src/main/java/net/iponweb/disthene/reader/service/metric/MetricService.java
+++ src/main/java/net/iponweb/disthene/reader/service/metric/MetricService.java
@@ -219,8 +219,11 @@ public class MetricService {
         List<Rollup> survivals = new ArrayList<>();
 
 	// DELETE ME -- BEGIN
-	long deployTime = 1541592000;
-	AbstractMap.SimpleEntry<Integer, Integer> desiredRoll = from > deployTime || to > deployTime ? 
+	long deployTime = 1541548800;
+	AbstractMap.SimpleEntry<Integer, Integer> desiredRoll = from > deployTime ? 
+							 new AbstractMap.SimpleEntry<>(60, 21600) : 
+					 		 new AbstractMap.SimpleEntry<>(3600, 360);
+	/*
 							 new AbstractMap.SimpleEntry<>(60, 21600) : 
 					 		 new AbstractMap.SimpleEntry<>(3600, 360);
 	/*
