4ab519381c1a1204b5b820665afff4a4082f0d42
swhors <swhors@daum.net>
2021-09-15 14:18:00 +0900

    DEV-0 Fix functions as python graphite function's results except HoltWinters

--- src/main/java/net/iponweb/disthene/reader/graphite/evaluation/TargetVisitor.java
+++ src/main/java/net/iponweb/disthene/reader/graphite/evaluation/TargetVisitor.java
@@ -47,7 +47,7 @@ public class TargetVisitor extends GraphiteBaseVisitor<Target> {
                 } else if (arg instanceof GraphiteParser.ArgNumberContext) {
                     function.addArg(Double.parseDouble(arg.getText()));
                 } else if (arg instanceof GraphiteParser.ArgStringContext) {
-                    function.addArg(arg.getText().replaceAll("^[\"\']|[\"\']$", ""));
+                    function.addArg(arg.getText().replaceAll("^[\"']|[\"']$", ""));
                 }
             }
 
