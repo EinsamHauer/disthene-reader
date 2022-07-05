abf2f0ef5b03fd5eaf513e5935f24ea6ed719e20
Andrei Ivanov <Andrei.B.Ivanov@gmail.com>
2022-07-05 17:51:37 +0300

    Fixed mimMax function

--- src/main/java/net/iponweb/disthene/reader/graphite/functions/MinMaxFunction.java
+++ src/main/java/net/iponweb/disthene/reader/graphite/functions/MinMaxFunction.java
@@ -24,8 +24,7 @@ public class MinMaxFunction extends DistheneFunction {
 
     @Override
     public List<TimeSeries> evaluate(TargetEvaluator evaluator) throws EvaluationException {
-        List<TimeSeries> processedArguments = new ArrayList<>();
-        processedArguments.addAll(evaluator.eval((Target) arguments.get(0)));
+        List<TimeSeries> processedArguments = new ArrayList<>(evaluator.eval((Target) arguments.get(0)));
 
         if (processedArguments.size() == 0) return new ArrayList<>();
 
@@ -36,14 +35,13 @@ public class MinMaxFunction extends DistheneFunction {
         int length = processedArguments.get(0).getValues().length;
 
         for (TimeSeries ts : processedArguments) {
-            List<Double> valuesArray = Arrays.asList(ts.getValues());
-            Double min = CollectionUtils.min(valuesArray);
-            Double max = CollectionUtils.max(valuesArray);
+            Double min = CollectionUtils.min(Arrays.asList(ts.getValues()));
+            Double max = CollectionUtils.max(Arrays.asList(ts.getValues()));
             if (min != null && max != null) {
-                Double range = max - min;
+                double range = max - min;
                 for (int i = 0; i < length; i++) {
-                    if (valuesArray[i] != null ) {
-                        ts.getValues()[i] = (valuesArray[i] - min) / range;
+                    if (ts.getValues()[i] != null ) {
+                        ts.getValues()[i] = (ts.getValues()[i] - min) / range;
                     }
                 }
             }
